326d4ba2a52aa5e30abdb9ccdec104241df8e6cd
NaLan ZeYu <nalanzeyu@gmail.com>
2025-12-08 10:44:49 +0000

    [gfx906] ignore numerical instability

--- vllm/config/model.py
+++ vllm/config/model.py
@@ -1826,19 +1826,16 @@ _FLOAT16_NOT_SUPPORTED_MODELS = {
 
 
 def _is_valid_dtype(model_type: str, dtype: torch.dtype):
-    if model_type in _FLOAT16_NOT_SUPPORTED_MODELS and dtype == torch.float16:  # noqa: E501, SIM103
-        return False
-
+    # NOTE(gfx906): ignore numerical instability
     return True
 
 
 def _check_valid_dtype(model_type: str, dtype: torch.dtype):
+    # NOTE(gfx906): ignore numerical instability
     if model_type in _FLOAT16_NOT_SUPPORTED_MODELS and dtype == torch.float16:
-        reason = _FLOAT16_NOT_SUPPORTED_MODELS[model_type]
-        raise ValueError(
-            f"The model type {model_type!r} does not support float16. Reason: {reason}"
-        )
-
+        logger.warning(f"Although using float16 in model type {model_type!r} "
+                       f"may introduce potential numerical instability issues."
+                       " gfx906 only supports float16, so we continue to use.")
     return True
 
 
